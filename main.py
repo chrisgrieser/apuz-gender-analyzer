9cd0f3f1985e2aa234a4cc23134f529b2fbcb329
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2023-12-16 18:26:37 +0100

    rename folder

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 """Main Module."""
 
+
+from pathlib import Path
+
+
 def main() -> None:
     """Execute main function."""
+    # years_with_register = list(range(2011, 2022 + 1))
+    # for year in years_with_register:
+    #     print(f"Register for {year} is available.")
+    filepath = Path("chronological-registers/2011.txt")
+    text = filepath.read_text()
+    find = text.find("Mouffe")
+    print(f"🪚 {find = }")
 
 
 if __name__ == "__main__":
